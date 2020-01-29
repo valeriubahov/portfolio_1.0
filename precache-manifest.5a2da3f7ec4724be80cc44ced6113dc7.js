28e693921bf009e6762cc5909d6dc8408a9bf6ac
Valeriu <bahovvaleriu@gmail.com>
2020-01-29 21:12:54 +0100

    Updates

---
+++ precache-manifest.5a2da3f7ec4724be80cc44ced6113dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe3b666a8160be61f75f7a5f8a57b22",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ec744de03ba9c1649bdc",
+    "url": "/portfolio/static/css/main.de024a27.chunk.css"
+  },
+  {
+    "revision": "746214e50a04ea345a0c",
+    "url": "/portfolio/static/js/2.1e9a541b.chunk.js"
+  },
+  {
+    "revision": "bc0c83fb5c6c8b946b94c2ec61dd7b90",
+    "url": "/portfolio/static/js/2.1e9a541b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec744de03ba9c1649bdc",
+    "url": "/portfolio/static/js/main.5182d1e9.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);

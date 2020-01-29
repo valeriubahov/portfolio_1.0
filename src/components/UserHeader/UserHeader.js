45a9b76d77623389e8020f8ac12d9fa2cf55a64f
Valeriu Bahov <valeriu.bahov@rgigroup.com>
2020-01-29 17:21:37 +0100

    Modify Header

--- src/components/UserHeader/UserHeader.js
+++ src/components/UserHeader/UserHeader.js
@@ -23,15 +23,10 @@ const UserHeader = ({ user }) => {
             </a>
           </h4>
           <p>{user.basics.label}</p>
-          <p>Coding in {user.basics.region}</p>
+          <p>Location {user.basics.region}</p>
           <p>{user.basics.yearsOfExperience} years of experience as a developer</p>
           <p>{user.basics.headline}</p>
-          <p>
-            Blog:{' '}
-            <a href={user.basics.blog} target="_blank" rel="noreferrer noopener">
-              {user.basics.blog}
-            </a>
-          </p>
+
         </div>
       </Header>
       <div>

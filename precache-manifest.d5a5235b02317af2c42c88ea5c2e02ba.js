f8c344974814e9304035e4cb1cf40a863db78e62
Valeriu Bahov <valeriu.bahov@rgigroup.com>
2020-01-30 16:36:10 +0100

    Updates

---
+++ precache-manifest.d5a5235b02317af2c42c88ea5c2e02ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8faf735d78c3bc7f9fbf6d8814569644",
+    "url": "https://valeriubahov.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "06884419426b1d33926e",
+    "url": "https://valeriubahov.github.io/portfolio/static/css/main.613a74b2.chunk.css"
+  },
+  {
+    "revision": "38a17bf268ab8b092650",
+    "url": "https://valeriubahov.github.io/portfolio/static/js/2.00435472.chunk.js"
+  },
+  {
+    "revision": "bc0c83fb5c6c8b946b94c2ec61dd7b90",
+    "url": "https://valeriubahov.github.io/portfolio/static/js/2.00435472.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06884419426b1d33926e",
+    "url": "https://valeriubahov.github.io/portfolio/static/js/main.90a7a078.chunk.js"
+  },
+  {
+    "revision": "5692659acb8d04210724",
+    "url": "https://valeriubahov.github.io/portfolio/static/js/runtime-main.520e7622.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.df0c5725fdbb6319e63fcec81205b690.js"
+  "https://valeriubahov.github.io/portfolio/precache-manifest.d5a5235b02317af2c42c88ea5c2e02ba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://valeriubahov.github.io/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

d2fbf1808877fb6fb9be52c721b202f952c4a1b0
Valeriu Bahov <valeriu.bahov@rgigroup.com>
2020-02-07 15:22:12 +0100

    Updates

---
+++ precache-manifest.c21ef4ec0b04e2572d80c562bf999be8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709d596e7cbdc76b185e7f9b356cb2bd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4fbb8ab6af2582c9f048",
+    "url": "/portfolio/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "6d330ad558eabbfd2639",
+    "url": "/portfolio/static/css/main.de024a27.chunk.css"
+  },
+  {
+    "revision": "4fbb8ab6af2582c9f048",
+    "url": "/portfolio/static/js/2.6b9633c0.chunk.js"
+  },
+  {
+    "revision": "bc0c83fb5c6c8b946b94c2ec61dd7b90",
+    "url": "/portfolio/static/js/2.6b9633c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d330ad558eabbfd2639",
+    "url": "/portfolio/static/js/main.9e35f20a.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ec8713f0129c4c3fb9f0676e2890ed6b.js"
+  "/portfolio/precache-manifest.c21ef4ec0b04e2572d80c562bf999be8.js"
 );
 
 self.addEventListener('message', (event) => {
